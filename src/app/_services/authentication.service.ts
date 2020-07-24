c7ee3151a0a535b57a5b9c7708de947baaad5e6f
Your global username <your@email.com>
2020-07-24 16:59:37 -0400

    to github

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -25,7 +25,8 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+        return this.http.post<any>(`${environment.apiUrl}ping`, { username, password })
+        //return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
             .pipe(map(user => {
                 // store user details and basic auth credentials in local storage to keep user logged in between page refreshes
                 user.authdata = window.btoa(username + ':' + password);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    //apiUrl: 'http://localhost:4000',
-    apiUrl:'https://afsaval.agenciasur.cl/webservice/rest/ping'
+    apiUrl: 'http://localhost:4000',
+    //apiUrl:'https://afsaval.agenciasur.cl/webservice/rest/ping'
 };
